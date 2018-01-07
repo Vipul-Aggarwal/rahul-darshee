71a5245fbf45e9c72fd4a2c02f398c72d65d46b5
Vipul Aggarwal <vipul.aggarwal@PW-C02Q4SDNG8WN.local>
2018-01-07 19:19:24 +0530

    refactor

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,72 +1,72 @@
 require 'json'
 class WelcomeController < ApplicationController
   def index
-    file = File.read('/Users/amith.prasad/Lobby/rahul-darshee/app/helpers/blessings.json')
+    file = File.read('/home/ubuntu/rahul-darshee/app/helpers/blessings.json')
     @blessings = JSON.parse(file)
 
     if !params.nil? and !params['welcome'].blank? and !params['welcome']['name'].blank?
       @blessings[params['welcome']['message']] = params['welcome']['name']
-      File.write('/Users/amith.prasad/Lobby/rahul-darshee/app/helpers/blessings.json', @blessings.to_json)
+      File.write('/home/ubuntu/rahul-darshee/app/helpers/blessings.json', @blessings.to_json)
       params['welcome']['name'] = ''
       params['welcome']['message'] = ''
     end
   end
 
   def when_where
-    file = File.read('/Users/amith.prasad/Lobby/rahul-darshee/app/helpers/blessings.json')
+    file = File.read('/home/ubuntu/rahul-darshee/app/helpers/blessings.json')
     @blessings = JSON.parse(file)
 
     if !params.nil? and !params['welcome'].blank? and !params['welcome']['name'].blank?
       @blessings[params['welcome']['message']] = params['welcome']['name']
-      File.write('/Users/amith.prasad/Lobby/rahul-darshee/app/helpers/blessings.json', @blessings.to_json)
+      File.write('/home/ubuntu/rahul-darshee/app/helpers/blessings.json', @blessings.to_json)
       params['welcome']['name'] = ''
       params['welcome']['message'] = ''
     end
   end
 
   def guest
-    file = File.read('/Users/amith.prasad/Lobby/rahul-darshee/app/helpers/blessings.json')
+    file = File.read('/home/ubuntu/rahul-darshee/app/helpers/blessings.json')
     @blessings = JSON.parse(file)
 
     if !params.nil? and !params['welcome'].blank? and !params['welcome']['name'].blank?
       @blessings[params['welcome']['message']] = params['welcome']['name']
-      File.write('/Users/amith.prasad/Lobby/rahul-darshee/app/helpers/blessings.json', @blessings.to_json)
+      File.write('/home/ubuntu/rahul-darshee/app/helpers/blessings.json', @blessings.to_json)
       params['welcome']['name'] = ''
       params['welcome']['message'] = ''
     end
   end
 
   def gallery
-    file = File.read('/Users/amith.prasad/Lobby/rahul-darshee/app/helpers/blessings.json')
+    file = File.read('/home/ubuntu/rahul-darshee/app/helpers/blessings.json')
     @blessings = JSON.parse(file)
 
     if !params.nil? and !params['welcome'].blank? and !params['welcome']['name'].blank?
       @blessings[params['welcome']['message']] = params['welcome']['name']
-      File.write('/Users/amith.prasad/Lobby/rahul-darshee/app/helpers/blessings.json', @blessings.to_json)
+      File.write('/home/ubuntu/rahul-darshee/app/helpers/blessings.json', @blessings.to_json)
       params['welcome']['name'] = ''
       params['welcome']['message'] = ''
     end
   end
 
   def blog
-    file = File.read('/Users/amith.prasad/Lobby/rahul-darshee/app/helpers/blessings.json')
+    file = File.read('/home/ubuntu/rahul-darshee/app/helpers/blessings.json')
     @blessings = JSON.parse(file)
 
     if !params.nil? and !params['welcome'].blank? and !params['welcome']['name'].blank?
       @blessings[params['welcome']['message']] = params['welcome']['name']
-      File.write('/Users/amith.prasad/Lobby/rahul-darshee/app/helpers/blessings.json', @blessings.to_json)
+      File.write('/home/ubuntu/rahul-darshee/app/helpers/blessings.json', @blessings.to_json)
       params['welcome']['name'] = ''
       params['welcome']['message'] = ''
     end
   end
 
   def groom_bride
-    file = File.read('/Users/amith.prasad/Lobby/rahul-darshee/app/helpers/blessings.json')
+    file = File.read('/home/ubuntu/rahul-darshee/app/helpers/blessings.json')
     @blessings = JSON.parse(file)
 
     if !params.nil? and !params['welcome'].blank? and !params['welcome']['name'].blank?
       @blessings[params['welcome']['message']] = params['welcome']['name']
-      File.write('/Users/amith.prasad/Lobby/rahul-darshee/app/helpers/blessings.json', @blessings.to_json)
+      File.write('/home/ubuntu/rahul-darshee/app/helpers/blessings.json', @blessings.to_json)
       params['welcome']['name'] = ''
       params['welcome']['message'] = ''
     end
